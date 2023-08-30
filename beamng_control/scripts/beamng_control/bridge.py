added a rotation to align the yawing of the vehicle with the road network. something is not quite correct tho becase the lidar is upsidown and proceesds in reverse?

beamng_control/scripts/beamng_control/bridge.py
@@ -13,6 +13,7 @@ import rospkg
 import actionlib
 import tf
 import tf2_ros
+from tf.transformations import quaternion_from_euler, quaternion_multiply
 import geometry_msgs.msg
 
 import beamngpy as bngpy
@@ -153,7 +154,6 @@ class BeamNGBridge(object):
             vehicle.attach_sensor(n_name, noise)
         return vehicle
 
-
     @staticmethod
     def get_stamped_static_tf_frame(translation, rotation, vehicle_name: str, sensor_name: str):
         static_transform_stamped = geometry_msgs.msg.TransformStamped()
@@ -168,6 +168,10 @@ class BeamNGBridge(object):
                                                         float(rotation[0]),
                                                         float(rotation[1]))  # RPY to convert
 
+        alignment_quat = np.array([0, 0, 0, 1])  # sets the forward direction as -y
+        # alignment_quat = np.array([0, 1, 0, 0])  # sets the forward direction as -y
+        quat = quaternion_multiply(alignment_quat, quat)
+        quat /= np.linalg.norm(quat)
         static_transform_stamped.transform.rotation.x = quat[0]
         static_transform_stamped.transform.rotation.y = quat[1]
         static_transform_stamped.transform.rotation.z = quat[2]
@@ -249,7 +253,8 @@ class BeamNGBridge(object):
             self._vehicle_publisher = VehiclePublisher(vehicle, NODE_NAME)  # we need this to be published first for tf
             scenario.add_vehicle(vehicle,
                                  pos=v_spec['position'],
-                                 rot_quat=v_spec['rotation'])
+                                 rot_quat=v_spec['rotation']
+                                 )
             vehicle_list.append(vehicle)
 
         on_scenario_start = list()
@@ -455,11 +460,12 @@ class BeamNGBridge(object):
         ros_rate = 10
         rate = rospy.Rate(ros_rate)  # todo add threading
         if self.running:
-            for static_tf in self._static_tf_frames:
-                static_tf.header.stamp = rospy.Time.now()
-                self._static_tf_broadcaster.sendTransform(static_tf)
+
             while not rospy.is_shutdown():
                 current_time = rospy.Time.now()
+                for static_tf in self._static_tf_frames:
+                    static_tf.header.stamp = current_time
+                    self._static_tf_broadcaster.sendTransform(static_tf)
                 if self._vehicle_publisher is not None:
                     self._vehicle_publisher.publish(current_time)
                 for pub in self._publishers:

beamng_control/scripts/beamng_control/publishers.py
@@ -16,6 +16,7 @@ import threading
 import sensor_msgs
 import geometry_msgs.msg as geom_msgs
 import std_msgs.msg
+from tf.transformations import quaternion_from_euler, quaternion_multiply
 
 import beamngpy.sensors as bng_sensors
 # from beamngpy.noise import RandomImageNoise, RandomLIDARNoise
@@ -453,6 +454,7 @@ class LidarPublisher(SensorDataPublisher):
             points = np.zeros((0, 3))
             colours = np.zeros((0,))
             trans_map = np.zeros(3)
+
         pointcloud_data = np.zeros(points.shape[0], dtype=pointcloud_fields)
         pointcloud_data['x'] = points[:, 0] - trans_map[0]
         pointcloud_data['y'] = points[:, 1] - trans_map[1]
@@ -475,6 +477,9 @@ class VehiclePublisher(BNGPublisher):
         self.frame_map = 'map'
         self.tf_msg.header.frame_id = self.frame_map
         self.tf_msg.child_frame_id = self._vehicle.vid
+        self.alignment_quat = np.array([0, 1, 0, 0])  # sets the forward direction as -y
+        # self.alignment_quat = np.array([0, 0, 0, 1])  # sets the forward direction as -y
+        # self.alignment_quat = np.array([1, 0, 0, 0])
         self.current_time = rospy.get_rostime()
 
         self.node_name = node_name
@@ -504,10 +509,18 @@ class VehiclePublisher(BNGPublisher):
         self.tf_msg.transform.translation.y = data['pos'][1]
         self.tf_msg.transform.translation.z = data['pos'][2]
 
-        self.tf_msg.transform.rotation.x = data['rotation'][0]
-        self.tf_msg.transform.rotation.y = data['rotation'][1]
-        self.tf_msg.transform.rotation.z = data['rotation'][2]
-        self.tf_msg.transform.rotation.w = data['rotation'][3]
+        quat_orientation = np.array([data['rotation'][0],
+                                     data['rotation'][1],
+                                     data['rotation'][2],
+                                     data['rotation'][3]])
+
+        quat_orientation = quaternion_multiply(self.alignment_quat, quat_orientation)
+        quat_orientation /= np.linalg.norm(quat_orientation)
+
+        self.tf_msg.transform.rotation.x = quat_orientation[0]  # data['rotation'][0]
+        self.tf_msg.transform.rotation.y = quat_orientation[1]  # data['rotation'][1]
+        self.tf_msg.transform.rotation.z = quat_orientation[2]  # data['rotation'][2]
+        self.tf_msg.transform.rotation.w = quat_orientation[3]  # data['rotation'][3]
         self._broadcaster_pose.sendTransform(self.tf_msg)
 
     def state_to_marker(self, data, marker_ns):